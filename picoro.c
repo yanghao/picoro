2b5ed9da3582ab5873beacf9f7a4f90946448835
Tony Finch <dot@dotat.at>
2010-02-02 00:01:46 +0000

    Better variable names.

--- picoro.c
+++ picoro.c
@@ -12,40 +12,40 @@
 #include "picoro.h"
 
 static struct coro {
-	jmp_buf state;
-} first, *running = &first, *prev, *idle;
+	jmp_buf buf;
+} first, *here = &first, *prev, *idle;
 
 coro corunning(void) {
-	return(running);
+	return(here);
 }
 
 void *coto(coro next, void *arg) {
 	static void *saved; saved = arg;
-	prev = running; running = next;
-	if(!setjmp(prev->state)) longjmp(next->state, 1);
+	prev = here; here = next;
+	if(!setjmp(prev->buf)) longjmp(next->buf, 1);
 	return(saved);
 }
 
 void coroutine_new(void), coroutine_starter(void*);
 
 coro coroutine(int fun(void *)) {
-	if(!idle && !setjmp(running->state)) coroutine_new();
+	if(!idle && !setjmp(here->buf)) coroutine_new();
 	return(coto(idle, fun));
 }
 
 void coroutine_new(void) {
-	char stack[16 * 1024];
-	coroutine_starter(stack);
+	char space[16 * 1024];
+	coroutine_starter(space);
 }
 
-void coroutine_starter(void *stack) {
+void coroutine_starter(void *space) {
 	int (*fun)(coro);
-	struct coro me, *parent = running;
-	running = idle = &me;
-	fun = coto(parent, stack);
-	parent = prev;
-	if(!setjmp(running->state)) coroutine_new();
-	exit(fun(coto(parent, &me)));
+	struct coro me, *back = here;
+	idle = here = &me;
+	fun = coto(back, space);
+	back = prev;
+	if(!setjmp(here->buf)) coroutine_new();
+	exit(fun(coto(back, &me)));
 }
 
 /* eof */
